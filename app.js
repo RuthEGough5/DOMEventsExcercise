b1d5891b5b58a449b5cf151d3b88ae280abf5b28
Ruth Gough <ruthegough5@gmail.com>
2021-11-01 07:44:09 -0500

    Completion of excercise, including bonus.

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
 // assignment questions:
-/*1a. Select the 1st button element and set it to a const variable called button1
-1b. Using the ONCLICK event property, have an alert that says "Cool! You can hear/understand a hummingbird and ants" pop up when you click on the button
-2a. Select the h3 and set it to a const variable called h3
-2b. Using addEventListener, have an alert that says "OH NO! The squirrels are on to you. Now you have to leave this reality and move to a new one" pop up when you hover over the h3
-3a. Select the form element/tag and set it to a const variable called form
-3b. Using addEventListener, display the value in the text input with an alert when the submit input in the form is pressed.*/
+/*1a. Select the 1st button element and set it to a const variable called button1*/
 
-/* assignment bonus questions:
-4a. Select the Dark Mode button and set it to a const variable called darkMode
-4b. Using addEventListener, TOGGLE the "dark-mode" CSS class On/Off for EVERY element when the Dark Mode Button is clicked (HINT: You will need to use the asterisk "*" selector as well as a loop)
-5a. Select the "MOVE TO ANOTHER REALITY" button and set it to a const variable called reality
-5b. Using addEventListener, have an alert that says "You have successfully moved to another reality" pop up when you click on the button. However, have this setup so that when the button is clicked a third time have the following alert display instead of the "You have successfully moved to another reality" alert. New alert "OH NO! You can only move to a new another reality a couple times. You are stuck in this reality!". And finally, when the button is clicked a fourth, fifth, sixth, etc. time have it do nothing (AKA Disable the button).*/
+const button1 = document.getElementById("one");
+
+/* 1b. Using the ONCLICK event property, have an alert that says "Cool! You can hear/understand a hummingbird and ants" pop up when you click on the button*/
+button1.onclick = function (){
+    alert("Cool! You can hear/understand a hummingbird and ants");
+  };
+
+/*2a. Select the h3 and set it to a const variable called h3*/
+
+const h3 = document.getElementsByTagName(h3);
+
+/*2b. Using addEventListener, have an alert that says "OH NO! The squirrels are on to you. Now you have to leave this reality and move to a new one" pop up when you hover over the h3 */
+h3.addEventListener(`mouseover`, function(){
+    alert ("OH NO! The squirrels are on to you. Now you have to leave this reality and move to a new one"));
+  });
+
+/*3a. Select the form element/tag and set it to a const variable called form */
+
+const form = document.querySelector(`form`);
+
+
+/*3b. Using addEventListener, display the value in the text input with an alert when the submit input in the form is pressed.*/
+
+form.addEventListener(`submit`, f => {
+    f.preventDefault();
+	const formInput = form.elements.quote.value;
+    alert(`${formInput}`);
+    form.elements.quote.value = ``;
+});
+
+//BONUS
+//4A
+const darkMode = document.querySelector(`#dm`);
+
+//4B
+darkMode.addEventListener(`click`, () => {
+    for (all of document.querySelectorAll(`*`))
+    {
+        all.classList.toggle(`dark-mode`);
+    }
+});
